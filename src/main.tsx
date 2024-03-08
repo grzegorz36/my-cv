521779268801e96afb73f7c0dbb05db369f25e82
Grzegorz Błaszczuk <grzegorz.blaszczuk36@gmail.com>
2024-03-08 17:06:16 +0100

    created interface

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+interface Experience {
+  description: string;
+  year: number;
+}
+interface CV {
+  photo: string;
+  name: string;
+  lastName: string;
+  position: string;
+  experience: Experience[];
+  education: string[];
+} 
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    
+
   </React.StrictMode>,
 )
